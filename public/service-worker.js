0139425ccd40e175ecf420c5b6787bf3a8322988
rice <minhrice.dev@gmail.com>
2024-10-21 23:15:24 +0700

    offline file

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,24 +1,19 @@
-// src/service-worker.js
 self.addEventListener("install", (event) => {
-  console.log("Service Worker installing.");
   event.waitUntil(
-    caches.open("static-v1").then((cache) => {
+    caches.open("my-cache").then((cache) => {
       return cache.addAll([
-        "./",
-        "./index.html",
-        "./favicon.svg",
-        "./pwa-192x192.png",
-        "./pwa-512x512.png",
+        "/offline.html", // Trang offline sẽ được hiển thị
       ]);
     })
   );
 });
 
 self.addEventListener("fetch", (event) => {
-  console.log("Service Worker fetching.", event.request.url);
   event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
+    fetch(event.request).catch(() => {
+      return caches.match(event.request).then((response) => {
+        return response || caches.match("/offline.html");
+      });
     })
   );
 });
