569415c87a4907dae05a1ee4ae3d699f3432c593
PhucLH <phuclh@rikkeisoft.com>
2024-10-30 10:36:15 +0700

    fix bug

--- src/components/Molecules/FloatButton/SocialMediaButton/index.tsx
+++ src/components/Molecules/FloatButton/SocialMediaButton/index.tsx
@@ -1,67 +1,67 @@
 import React, { useEffect, useId, useState } from "react";
-import { Box, Image } from "@/components/Atoms";
+import { Box, Button, Image } from "@/components/Atoms";
 import { FileHelpers } from "@/helpers";
 import { Tooltip } from "@/components/Atoms";
 
 interface SocialMediaItem {
-    icon: string;
-    id: string;
-    link: string;
-    text: string;
+  icon: string;
+  id: string;
+  link: string;
+  text: string;
 }
 
 const SocialMediaButton: React.FC = () => {
-    const data: SocialMediaItem[] = [
-        {
-            icon: "facebook",
-            id: "facebook",
-            link: "https://www.facebook.com/rice2411/",
-            text: "Liên hệ tại Facebook",
-        },
-        {
-            icon: "zalo",
-            id: "zalo",
-            link: "https://zalo.me/0776750418",
-            text: "Liên hệ tại Zalo",
-        },
-    ];
+  const data: SocialMediaItem[] = [
+    {
+      icon: "facebook",
+      id: "facebook",
+      link: "https://www.facebook.com/rice2411/",
+      text: "Liên hệ tại Facebook",
+    },
+    {
+      icon: "zalo",
+      id: "zalo",
+      link: "https://zalo.me/0776750418",
+      text: "Liên hệ tại Zalo",
+    },
+  ];
 
-    const [position, setPosition] = useState<string>("bottom-5");
+  const [position, setPosition] = useState<string>("bottom-5");
 
-    const handleClick = (id: string) => {
-        const item = data.find((item) => item.id === id);
-        if (item) {
-            window.open(item.link, "_blank");
-        }
+  const handleClick = (id: string) => {
+    const item = data.find((item) => item.id === id);
+    if (item) {
+      window.open(item.link, "_blank");
+    }
+  };
+
+  useEffect(() => {
+    const handleScroll = () => {
+      setPosition(window.scrollY > 300 ? "bottom-24" : "bottom-5");
     };
 
-    useEffect(() => {
-        const handleScroll = () => {
-            setPosition(window.scrollY > 300 ? "bottom-24" : "bottom-5");
-        };
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
 
-        window.addEventListener("scroll", handleScroll);
-        return () => window.removeEventListener("scroll", handleScroll);
-    }, []);
-
-    return (
-        <Box className={`flex flex-col fixed right-5 ${position}`}>
-            {data.map((item) => (
-                <Tooltip text={item.link} position="left" key={useId()}>
-                    <Box
-                        className="cursor-pointer flex justify-center items-center w-14 h-14 bg-white text-black p-3 rounded-full shadow-lg mt-3"
-                        aria-label="Social media link"
-                        onClick={() => handleClick(item.id)}
-                    >
-                        <Image
-                            src={FileHelpers.getLocalFile(item.icon, "png")}
-                            className="h-7 w-7"
-                        />
-                    </Box>
-                </Tooltip>
-            ))}
-        </Box>
-    );
+  return (
+    <Box className={`flex flex-col fixed right-5 ${position}`}>
+      {data.map((item) => (
+        <Tooltip text={item.link} position="left" key={useId()}>
+          <Button
+            className="cursor-pointer  w-14 h-14 bg-white text-black p-3 !rounded-full shadow-lg mt-3"
+            aria-label="Social media link"
+            onClick={() => handleClick(item.id)}
+          >
+            <Image
+              src={FileHelpers.getLocalFile(item.icon, "png")}
+              className="h-6 w-6"
+            />
+          </Button>
+        </Tooltip>
+      ))}
+    </Box>
+  );
 };
 
 export default SocialMediaButton;
--- src/components/Molecules/Pagination/index.tsx
+++ src/components/Molecules/Pagination/index.tsx
@@ -4,104 +4,88 @@ import { PAGINATION_CONSTANTS } from "@/constants";
 import { FileHelpers } from "@/helpers";
 
 interface PaginationProps {
-    page: number;
-    setPage: (page: number) => void;
-    length: number;
+  page: number;
+  setPage: (page: number) => void;
+  length: number;
 }
 
 const inactiveClass = `flex items-center justify-center text-sm py-2 px-3 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700`;
 const activeClass = `flex items-center justify-center text-sm z-10 py-2 px-3 leading-tight text-primary-600 bg-primary-50 border border-primary-300 hover:bg-primary-100 hover:text-primary-700`;
 
 const Pagination: React.FC<PaginationProps> = ({ page, setPage, length }) => {
-    if (!length) return null;
+  if (!length) return null;
 
-    return (
+  return (
+    <Box
+      className="flex flex-col md:flex-row justify-between items-start md:items-center space-y-3 md:space-y-0 p-4"
+      aria-label="Table navigation"
+    >
+      <Box className="text-sm font-normal text-gray-500 flex">
+        Showing
+        <Paragraph className="font-semibold text-gray-900 mx-1">
+          {Math.min(
+            (page - 1) * PAGINATION_CONSTANTS.DEFAULT_PAGE_SIZE + 1,
+            length
+          )}{" "}
+          - {Math.min(page * PAGINATION_CONSTANTS.DEFAULT_PAGE_SIZE, length)}
+        </Paragraph>
+        of
+        <Paragraph className="font-semibold text-gray-900 ml-1">
+          {length}
+        </Paragraph>
+      </Box>
+      <Box className="inline-flex items-stretch -space-x-px cursor-pointer ">
         <Box
-            className="flex flex-col md:flex-row justify-between items-start md:items-center space-y-3 md:space-y-0 p-4"
-            aria-label="Table navigation"
+          onClick={() => {
+            if (page === 1) return;
+            setPage(page - 1);
+          }}
+          className="flex items-center justify-center py-1.5 px-3 ml-0 text-gray-500 bg-white rounded-l-lg border border-gray-300 hover:bg-gray-100 hover:text-gray-700 "
         >
-            <Box className="text-sm font-normal text-gray-500 flex">
-                Showing
-                <Paragraph className="font-semibold text-gray-900 mx-1">
-                    {Math.min(
-                        (page - 1) * PAGINATION_CONSTANTS.DEFAULT_PAGE_SIZE + 1,
-                        length
-                    )}{" "}
-                    -{" "}
-                    {Math.min(
-                        page * PAGINATION_CONSTANTS.DEFAULT_PAGE_SIZE,
-                        length
-                    )}
-                </Paragraph>
-                of
-                <Paragraph className="font-semibold text-gray-900 ml-1">
-                    {length}
-                </Paragraph>
-            </Box>
-            <Box className="inline-flex items-stretch -space-x-px cursor-pointer ">
-                <Box
-                    onClick={() => {
-                        if (page === 1) return;
-                        setPage(page - 1);
-                    }}
-                    className="flex items-center justify-center py-1.5 px-3 ml-0 text-gray-500 bg-white rounded-l-lg border border-gray-300 hover:bg-gray-100 hover:text-gray-700 "
-                >
-                    <Paragraph className="sr-only">Previous</Paragraph>
-                    <Svg
-                        className="w-5 h-5"
-                        fill="currentColor"
-                        viewBox="0 0 20 20"
-                        src={FileHelpers.getLocalFile(
-                            "pagination-left-arrow",
-                            "path"
-                        )}
-                    />
-                </Box>
-                {Array(
-                    Math.ceil(length / PAGINATION_CONSTANTS.DEFAULT_PAGE_SIZE)
-                )
-                    .fill(null)
-                    .map((_, index) => (
-                        <Box
-                            key={index}
-                            className={
-                                page === index + 1 ? activeClass : inactiveClass
-                            }
-                            onClick={() => {
-                                setPage(index + 1);
-                            }}
-                        >
-                            {index + 1}
-                        </Box>
-                    ))}
-
-                <Box
-                    onClick={() => {
-                        if (
-                            page ===
-                            Math.ceil(
-                                length / PAGINATION_CONSTANTS.DEFAULT_PAGE_SIZE
-                            )
-                        )
-                            return;
-                        setPage(page + 1);
-                    }}
-                    className="cursor-pointer flex items-center justify-center py-1.5 px-3 leading-tight text-gray-500 bg-white rounded-r-lg border border-gray-300 hover:bg-gray-100 hover:text-gray-700 "
-                >
-                    <Paragraph className="sr-only">Next</Paragraph>
-                    <Svg
-                        className="w-5 h-5"
-                        fill="currentColor"
-                        viewBox="0 0 20 20"
-                        src={FileHelpers.getLocalFile(
-                            "pagination-right-arrow",
-                            "path"
-                        )}
-                    />
-                </Box>
-            </Box>
+          <Paragraph className="sr-only">Previous</Paragraph>
+          <Svg
+            className="w-5 h-5"
+            fill="currentColor"
+            viewBox="0 0 20 20"
+            src={FileHelpers.getLocalFile("pagination-left-arrow", "path")}
+          />
         </Box>
-    );
+        {Array(Math.ceil(length / PAGINATION_CONSTANTS.DEFAULT_PAGE_SIZE))
+          .fill(null)
+          .map((_, index) => (
+            <Box
+              key={index}
+              className={page === index + 1 ? activeClass : inactiveClass}
+              onClick={() => {
+                setPage(index + 1);
+              }}
+            >
+              {index + 1}
+            </Box>
+          ))}
+
+        <Box
+          onClick={() => {
+            if (
+              page ===
+              Math.ceil(length / PAGINATION_CONSTANTS.DEFAULT_PAGE_SIZE)
+            )
+              return;
+            setPage(page + 1);
+          }}
+          className="cursor-pointer flex items-center justify-center py-1.5 px-3 leading-tight text-gray-500 bg-white rounded-r-lg border border-gray-300 hover:bg-gray-100 hover:text-gray-700 "
+        >
+          <Paragraph className="sr-only">Next</Paragraph>
+          <Svg
+            className="w-5 h-5"
+            fill="currentColor"
+            viewBox="0 0 20 20"
+            src={FileHelpers.getLocalFile("pagination-right-arrow", "path")}
+          />
+        </Box>
+      </Box>
+    </Box>
+  );
 };
 
 export default Pagination;
