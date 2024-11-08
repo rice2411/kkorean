ca639a646ebcf908cc0b25f9536353a9f1dcc7ab
rice <minhrice.dev@gmail.com>
2024-11-08 20:48:37 +0700

    fix bug button hover

--- src/components/Organisms/Section/Hero/index.tsx
+++ src/components/Organisms/Section/Hero/index.tsx
@@ -70,9 +70,9 @@ function HeroSection() {
           Chúng mình có rất nhiều bộ để để cải thiện kỹ năng nghe và đọc của bạn
           cho các đề thi TOPIK
         </Paragraph>
-        <Box className="flex flex-col mb-8 lg:mb-16 space-y-4 sm:flex-row sm:justify-center sm:space-y-0 sm:space-x-4">
+        <Box className="flex  flex-col mb-8 lg:mb-16 space-y-4 sm:flex-row sm:justify-center sm:space-y-0 sm:space-x-4">
           <Button
-            className="px-5 py-3"
+            className="px-5 py-3 z-[9999999]"
             hover={true}
             onClick={() => {
               navigate("/exam");
